2e2a87ea3641234ed1b598fc68cab33a336bc2ae
Biblio-Khan <agendamentosalas00@gmail.com>
2026-05-28 08:41:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.sidebar.image("logo_bibliokhan.png", use_container_width=True)
  # --- BARRA LATERAL (Tudo encostado na esquerda) ---
 with st.sidebar:
     st.title("**BiblioKhan**")                      # 4 espaços de recuo
-    st.write("*Inteligência e automação para Bibliotecas*")
+    st.write("**Inteligência e automação para Bibliotecas**")
     st.write("email")                           # 4 espaços de recuo
     st.markdown("---")                          # 4 espaços de recuo
 
