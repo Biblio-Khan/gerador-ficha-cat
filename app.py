844946756bca1d3961566a3e66e9e483a7fa71ac
Biblio-Khan <bibliokhancontato@gmail.com>
2026-05-31 00:01:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ else:
     """ #  Agora tem 4 espaços (ou 1 Tab) de recuo!
     Busca o número Cutter correto a partir do CSV no GitHub.
     """
-    URL_CUTTER_CSV = "https://raw.githubusercontent.com/..."
+    URL_CUTTER_CSV = "raw.githubusercontent.com/Biblio-Khan/gerador-ficha-cat/refs/heads/main/cutter.csv"
     
     # Se o sobrenome ou título estiverem vazios, não faz a busca
     if not sobrenome_autor or not titulo_obra:
