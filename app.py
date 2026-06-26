d64a7dd4a83e53973a639eca1000da0d1cdc0c2c
Biblio-Khan <bibliokhancontato@gmail.com>
2026-06-26 11:30:35 -0300

    Fix formatting issues in marc_lines list

--- app.py
+++ app.py
@@ -378,8 +378,8 @@ else:
             "000 00000nam a2200000 i 4500",
             f"100 1#$a{dados.get('entrada', '')}",
             f"245 10$a{dados.get('titulo', '')}",
-            f"260 ##$a{dados.get('local_editora', 'Brasília : s.n.')}"
-            f"300 ##$a{dados.get('paginas', 'p.')} ; {dados.get('dimensoes', 'cm.')}")
+            f"260 ##$a{dados.get('local_editora', 'Brasília : s.n.')}", # Vírgula aqui!
+            f"300 ##$a{dados.get('paginas', 'p.')} ; {dados.get('dimensoes', 'cm.')}" # Sem parêntese extra!
         ]
     
         # Nota de Tese/Dissertação (Tag 502)
