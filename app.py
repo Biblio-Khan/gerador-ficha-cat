8e0540d34537a646b73d4d91939f22e223336fc5
Biblio-Khan <bibliokhancontato@gmail.com>
2026-07-02 17:30:26 -0300

    Fix validation check for 'valido' variable

--- app.py
+++ app.py
@@ -690,7 +690,7 @@ else:
                 valido = True
             if tipo_autor == "Pessoa Física" and not any(a.strip() for a in autores_lista) and not tem_organizador:
                 valido = False
-            if 'valido' not in locals()
+            if 'valido' not in locals():
                 valido = false
     
             if valido and titulo.strip():
