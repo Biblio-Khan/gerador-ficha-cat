37b80c53af494f2ef4ed344223a18d9f31b0c7a2
Biblio-Khan <bibliokhancontato@gmail.com>
2026-08-06 14:46:19 -0300

    Remove comment in finance tab section
    Removed comment about the rest of the code in the finance tab.

--- app.py
+++ app.py
@@ -764,7 +764,6 @@ else:
 # Abaixo, fora de qualquer bloco 'if' ou 'try', começa o tab_financeiro
     with tab_financeiro:
         st.header("💳 Gestão Financeira e Saldo")
-        # ... resto do seu código da aba
         col_f1, col_f2 = st.columns(2)
     
     with col_f1:
